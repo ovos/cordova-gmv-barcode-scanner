15ce82942aa6738fa4e8572d72b3fd46e236c616
Patrick Hübl-Neschkudla <pn@ovos.at>
2019-07-01 14:29:49 +0200

    ios: add support for colored codes

--- src/ios/CameraViewController.m
+++ src/ios/CameraViewController.m
@@ -20,7 +20,6 @@
 
 #import "CameraViewController.h"
 
-
 @interface CameraViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property(nonatomic, weak) IBOutlet UIView *placeHolderView;
@@ -37,8 +36,87 @@
 
 @end
 
+@implementation UIImage (NegativeImage)
+
+- (UIImage *)negativeImage
+{
+    // get width and height as integers, since we'll be using them as
+    // array subscripts, etc, and this'll save a whole lot of casting
+    CGSize size = self.size;
+    int width = size.width;
+    int height = size.height;
+    
+    // Create a suitable RGB+alpha bitmap context in BGRA colour space
+    CGColorSpaceRef colourSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *memoryPool = (unsigned char *)calloc(width*height*4, 1);
+    CGContextRef context = CGBitmapContextCreate(memoryPool, width, height, 8, width * 4, colourSpace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast);
+    CGColorSpaceRelease(colourSpace);
+    
+    // draw the current image to the newly created context
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), [self CGImage]);
+    
+    // run through every pixel, a scan line at a time...
+    for(int y = 0; y < height; y++)
+    {
+        // get a pointer to the start of this scan line
+        unsigned char *linePointer = &memoryPool[y * width * 4];
+        
+        // step through the pixels one by one...
+        for(int x = 0; x < width; x++)
+        {
+            // get RGB values. We're dealing with premultiplied alpha
+            // here, so we need to divide by the alpha channel (if it
+            // isn't zero, of course) to get uninflected RGB. We
+            // multiply by 255 to keep precision while still using
+            // integers
+            int r, g, b;
+            if(linePointer[3])
+            {
+                r = linePointer[0] * 255 / linePointer[3];
+                g = linePointer[1] * 255 / linePointer[3];
+                b = linePointer[2] * 255 / linePointer[3];
+            }
+            else
+                r = g = b = 0;
+            
+            // perform the colour inversion
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+            
+            // multiply by alpha again, divide by 255 to undo the
+            // scaling before, store the new values and advance
+            // the pointer we're reading pixel data from
+            linePointer[0] = r * linePointer[3] / 255;
+            linePointer[1] = g * linePointer[3] / 255;
+            linePointer[2] = b * linePointer[3] / 255;
+            linePointer += 4;
+        }
+    }
+    
+    // get a CG image from the context, wrap that into a
+    // UIImage
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    UIImage *returnImage = [UIImage imageWithCGImage:cgImage];
+    
+    // clean up
+    CGImageRelease(cgImage);
+    CGContextRelease(context);
+    free(memoryPool);
+    
+    // and return
+    return returnImage;
+}
+
+@end
+
 @implementation CameraViewController
 @synthesize delegate;
+
+- (CameraViewController *)init {
+    _isInverted = false;
+    return self;
+}
 
 - (BOOL)prefersStatusBarHidden
 {
@@ -148,6 +226,10 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     
     //We're going to crop UIImage to the onscreen viewfinder's box size for faster processing.
     UIImage *croppedImg = nil;
+    
+    //We want to test an inverted version every second time
+    UIImage *invertedImg = nil;
+
     //Rotate the image.
     UIImage * portraitImage = [[UIImage alloc] initWithCGImage: image.CGImage
                                                          scale: 1.0
@@ -178,6 +260,14 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     CGRect cropRect = CGRectMake(imageWidth/2 - actualFrameWidth/2, imageHeight/2 - actualFrameHeight/2, actualFrameWidth, actualFrameHeight);
     //Crop image
     croppedImg = [self croppIngimageByImageName:portraitImage toRect:cropRect];
+
+    // invert image colors
+    if(!_isInverted) {
+        croppedImg = [croppedImg negativeImage];
+        _isInverted = true;
+    } else {
+        _isInverted = false;
+    }
     
     //Test code to place the resultant cropped image onto the display to verify sizing.
     /*dispatch_async(dispatch_get_main_queue(), ^{
@@ -547,8 +637,4 @@ static inline double radians (double degrees) {return degrees * M_PI/180;}
     return nil;
 }
 
-
-
 @end
-
-
